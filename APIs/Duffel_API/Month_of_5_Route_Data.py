8853eb9ff15339dff42ff279b32754e33257b2b2
CooperChes <coopersalts@gmail.com>
2025-07-23 16:14:51 -0700

    Cleaned up Month_of_5_Route_Data.py and used Duffel_Calls_Library.py

--- APIs/Duffel_API/Month_of_5_Route_Data.py
+++ APIs/Duffel_API/Month_of_5_Route_Data.py
@@ -1,32 +1,35 @@
-# This code is very messy, I can clean it up later
-
 # This is the first program in the sequence "Month_of_5_Route_Data.py" -> "Gather_Possible_Routings.py" -> "Analyze_for_Synthetic_Routing.py"
 # It doesn't take any CSV files as inputs
 
 
-import requests
+
+
+
+import Duffel_Calls_Library as dcl
+import sqlite3
 import os
 from dotenv import load_dotenv
 from datetime import date, timedelta
-import pandas as pd
-import time
-import sqlite3
 
-
-DAYS_OF_DATA = 30
 
 if __name__ == "__main__":
-    # The information we want to save for each flight is:
+    OUTPUT_FILE = "Duffel_API/offers.csv"
+
+    START_DATE = '2025-07-25'
+    END_DATE = '2025-07-25'
+    '''
+    Dubai to London (DXB - LHR)
+    Singapore to Tokyo (SIN - NRT)
+    New York to San Francisco (JFK - SFO)
+    San Francisco to Tokyo (SFO - NRT)
+    Miami to Bogotá (MIA - BOG)
+    '''
+    ITINERARIES = [("DXB", "LHR"), ("SIN", "NRT"), ("JFK", "SFO"), ("SFO", "NRT"), ("MIA", "BOG")]
+
+    
+    # The "segments" key leads to a list containing the abbreviations of (for each leg of the flight):
         # The origin
         # The destination
-        # The date
-        # The carrier name
-        # The carrier code
-        # The price
-        # The currency
-        # For each segment (if there are layovers), a list containing the abbreviations of:
-            # The origin
-            # The destination
     data_csv = {
         "origin": [],
         "destination": [],
@@ -40,25 +43,14 @@ if __name__ == "__main__":
 
     load_dotenv()
     access_token = os.getenv("ACCESS_TOKEN")
-
-
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/json",
         "Duffel-Version": "v2"
     }
 
-    '''
-    Dubai to London (DXB - LHR)
-    Singapore to Tokyo (SIN - NRT)
-    New York to San Francisco (JFK - SFO)
-    San Francisco to Tokyo (SFO - NRT)
-    Miami to Bogotá (MIA - BOG)
-    '''
-
-    airlines = [("DXB", "LHR"), ("SIN", "NRT"), ("JFK", "SFO"), ("SFO", "NRT"), ("MIA", "BOG")]
-    start_date = date.today() + timedelta(days=1)
-    end_date = date.today() + timedelta(days=DAYS_OF_DATA)
+    start_date = date.fromisoformat(START_DATE)
+    end_date = date.fromisoformat(END_DATE)
 
     # Set up SQLite database
     conn = sqlite3.connect('Duffel_Flights.db')
@@ -78,7 +70,7 @@ if __name__ == "__main__":
     ''')
     conn.commit()
 
-    for origin, destination in airlines:
+    for origin, destination in ITINERARIES:
         departure_date = start_date
 
         while departure_date <= end_date:
@@ -95,73 +87,44 @@ if __name__ == "__main__":
                     "cabin_class": "economy"
                 }
             }
+            
+            offers = dcl.call_flight_offers(headers=headers, data=data)
+            
+            if offers:
+                for offer in offers:
+                    carrier = offer["owner"]
+                    airline_name = carrier["name"]
+                    airline_code = carrier["iata_code"]
+                    total_amount = offer["total_amount"]
+                    total_currency = offer["total_currency"]
 
-            response = requests.post(
-                "https://api.duffel.com/air/offer_requests",
-                headers=headers,
-                json=data
-            )
+                    # For all the segments (we put a "[0]" after "[slices]" because it is a one way ticket):
+                    segments = offer["slices"][0]["segments"]
+                    segment_origin_destination_pairs = []
+                    for leg in segments:
+                        leg_pair = [leg["origin"]["iata_code"], leg["destination"]["iata_code"]]
+                        segment_origin_destination_pairs.append(leg_pair)
 
-            # Sometimes there is a problem with the rate limit; this if statement adjusts for that
-            if response.status_code == 429:
-                print("Rate limit exceeded; waiting until it cools down")
-                start_time = time.time()
-
-                # We wait at most a minute
-                while time.time() - start_time < 60:
-                    time.sleep(1)
-                    response = requests.post(
-                        "https://api.duffel.com/air/offer_requests",
-                        headers=headers,
-                        json=data
-                    )
-
-                    if response.status_code == 429:
-                        pass
-                    else:
-                        break
-
-            if response.status_code == 201:
-                offers = response.json()["data"]["offers"]
-                if not offers:
-                    print(f"No offers found for {origin} to {destination} on {departure_str}.")
-                else:
-                    print(f"{len(offers)} offers found for {origin} to {destination} on {departure_str}:")
-                    for offer in offers:
-                        carrier = offer["owner"]
-                        # carrier = offer["slices"][0]["segments"][0]["operating_carrier"]
-                        airline_name = carrier["name"]
-                        airline_code = carrier["iata_code"]
-                        total_amount = offer["total_amount"]
-                        total_currency = offer["total_currency"]
-
-                        # For all the segments (we put a "[0]" after "[slices]" because it is a one way ticket):
-                        segments = offer["slices"][0]["segments"]
-                        segment_origin_destination_pairs = []
-                        for leg in segments:
-                            leg_pair = [leg["origin"]["iata_code"], leg["destination"]["iata_code"]]
-                            segment_origin_destination_pairs.append(leg_pair)
-
-                        data_append = [origin,
-                                       destination,
-                                       departure_str,
-                                       airline_name,
-                                       airline_code,
-                                       total_amount,
-                                       total_currency,
-                                       str(segment_origin_destination_pairs)]
-
-                        c.execute('''
+                    data_append = [origin,
+                                    destination,
+                                    departure_str,
+                                    airline_name,
+                                    airline_code,
+                                    total_amount,
+                                    total_currency,
+                                    str(segment_origin_destination_pairs)]
+                    
+                    c.execute('''
                             INSERT INTO flights (origin, destination, date, airline_name, airline_code, total_amount, total_currency, segments)
                             VALUES (?, ?, ?, ?, ?, ?, ?, ?)
                         ''', data_append)
-                        conn.commit()
-                        for i, key in enumerate(data_csv.keys()):
-                            data_csv[key].append(data_append[i])
-            else:
-                print(f"Error for {origin} to {destination} on {departure_str}: {response.status_code}")
-                print(response.text)
+                    conn.commit()
 
             departure_date += timedelta(days=1)
 
+
     conn.close()
+
+
+
+
