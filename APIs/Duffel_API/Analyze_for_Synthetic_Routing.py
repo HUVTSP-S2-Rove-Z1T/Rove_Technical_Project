ffe990dc670c123c8700c3bbcf818990b9315795
CooperChes <coopersalts@gmail.com>
2025-07-23 16:18:01 -0700

    Cleaned up Analyze_for_Synthetic_Routing.py and used Duffel_Calls_Library.py

--- APIs/Duffel_API/Analyze_for_Synthetic_Routing.py
+++ APIs/Duffel_API/Analyze_for_Synthetic_Routing.py
@@ -1,26 +1,18 @@
-# This code is very messy, I can clean it up later
-
 # This is the third program in the sequence "Month_of_5_Route_Data.py" -> "Gather_Possible_Routings.py" -> "Analyze_for_Synthetic_Routing.py"
 # It takes the CSV output of "Month_of_5_Route_Data.py" as the input ORIGINAL_INPUT_FILE and the CSV output of "Gather_Possible_Routings.py"
 # as the input ROUTING_INPUT_FILE
 
 
-import pandas as pd
-import ast
-from dotenv import load_dotenv
-import os
-import requests
-import time
-import matplotlib.pyplot as plt
-import numpy as np
+
+
+import Duffel_Calls_Library as dcl
 import sqlite3
+import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 
-# ORIGINAL_INPUT_FILE = 'Duffel_API/Month_of_5.csv'
-# ORIGINAL_INPUT_FILE = 'Duffel_API/Day_of_5.csv'  # Just checking one day keeps computation time reasonable; it takes a lot of Duffel calls to do synthetic routing
-
-# ROUTING_INPUT_FILE = 'Duffel_API/Day_of_Possible_Routings_5.csv'  # This should be the output of "Gather Possible Routings.py" on ORIGINAL_INPUT_FILE
 
 # Read from SQLite DB instead of CSV
 conn = sqlite3.connect('Duffel_Flights.db')
@@ -44,94 +36,79 @@ for row in routing_rows:
 conn.close()
 
 
-import ast
 segment_list = original_data_dict['segments']
 for i in range(len(segment_list)):
-    segment_list[i] = ast.literal_eval(segment_list[i])
+    segment_with_double_quotes = segment_list[i].replace("\'", "\"")
+    segment_list[i] = json.loads(segment_with_double_quotes)
 segment_list = routing_data_dict['segments']
 for i in range(len(segment_list)):
-    segment_list[i] = ast.literal_eval(segment_list[i])
+    segment_with_double_quotes = segment_list[i].replace("\'", "\"")
+    segment_list[i] = json.loads(segment_with_double_quotes)
 
 
-def get_cheapest_flight_index_and_amount(flight_dict_list):
-    min_index = 0
-    min_value = flight_dict_list[min_index]['total_amount']
-    for i in range(len(flight_dict_list)):
-        current_value = flight_dict_list[i]['total_amount']
-        if current_value < min_value:
-            min_value = current_value
-            min_index = i
-    
-    return [min_index, min_value]
 
-
-def get_cheapest_leg_order_index_and_amount(leg_order_price_list):
-    min_index = 0
-    min_value = leg_order_price_list[min_index]
-    for i in range(len(leg_order_price_list)):
-        current_value = leg_order_price_list[i]
-        if current_value < min_value:
-            min_value = current_value
-            min_index = i
-    
-    return [min_index, min_value]
-
-
-# "leg_list" should be a list of pairs of [origin, destination]. This needs to be seriously reworked, I think it works but is very messy
+# "leg_list" should be a list of pairs of [origin, destination]
 def get_all_valid_leg_orders(root_origin, root_destination, leg_list):
-    # This function returns all the possible ways that different legs can be put together to create the final trip.
-    # This operates under the hopefully safe assumption that if [City A, City B] is in the list, [City B, City A] is not. Also
-    # no larger cycles like [A, B], [B, C], and [C, A]
+    # This function returns all the possible ways that different legs can be put together to create the final trip. It searches in a tree until
+    # all combinations are exhausted
     total_legs = len(leg_list)
 
     all_valid_leg_orders = []
 
-    # It searches in a tree
-    current_leg_indices = [0]
-    while current_leg_indices[-1] < total_legs and leg_list[current_leg_indices[-1]][0] != root_origin:  # Make sure the first leg starts at "root_origin"
-        current_leg_indices[-1] += 1
-
-    if current_leg_indices[-1] >= total_legs:  # This should never happen
-        print('Error: none of the legs begin at the root_origin')
-        return []
-
-    add_new_leg = True
-    while len(current_leg_indices) > 0:
-        # Go deeper; add legs
-        if add_new_leg:
-            current_leg_indices.append(0)
-        
-        add_new_leg = True
-        if len(current_leg_indices) > 1:
-            while current_leg_indices[-1] < total_legs and leg_list[current_leg_indices[-1]][0] != leg_list[current_leg_indices[-2]][1]:
-                current_leg_indices[-1] += 1
+    # We set up the main operations on the tree; they don't return anything, but change the list because it is mutable
+    def append_zero(leg_indices):
+        leg_indices.append(0)
+    def increment(leg_indices):
+        leg_indices[-1] += 1
+    def increment_until_change(leg_indices):
+        # This keeps incrementing until index -1 either has the same origin as the destination of index -2, or until index -1 has cycled through all legs
+        if len(leg_indices) < 2:
+            target_origin = root_origin
         else:
-            while current_leg_indices[-1] < total_legs and leg_list[current_leg_indices[-1]][0] != root_origin:
-                current_leg_indices[-1] += 1
+            penultimate_leg_index = leg_indices[-2]
+            target_origin = leg_list[penultimate_leg_index][1]
+        
+        while True:
+            last_leg_index = leg_indices[-1]
+            if last_leg_index >= total_legs:
+                break
 
-        if current_leg_indices[-1] >= total_legs:  # If they have all been searched, remove a leg and update the previous leg as many times as necessary
-            while current_leg_indices[-1] >= total_legs:
-                current_leg_indices.pop()
-                if len(current_leg_indices) == 0:
+            last_origin = leg_list[last_leg_index][0]
+            if last_origin == target_origin:
+                if last_leg_index not in leg_indices[:-1]:  # No loops allowed, not that they should ever come up, but better to be safe
                     break
-                else:
-                    current_leg_indices[-1] += 1
-                    if len(current_leg_indices) == 1:
-                        while current_leg_indices[-1] < total_legs and leg_list[current_leg_indices[-1]][0] != root_origin:
-                            current_leg_indices[-1] += 1
-                    else:
-                        while current_leg_indices[-1] < total_legs and leg_list[current_leg_indices[-1]][0] != leg_list[current_leg_indices[-2]][1]:
-                            current_leg_indices[-1] += 1
-            add_new_leg = False
-
-        else:  # Then a leg clicked, and the tree can keep growing
-            if leg_list[current_leg_indices[-1]][1] == root_destination:
-                all_valid_leg_orders.append(current_leg_indices.copy())
-                current_leg_indices[-1] += 1
-                add_new_leg = False
-            else:
-                pass
+            
+            leg_indices[-1] += 1
+    def remove_last(leg_indices):
+        leg_indices.pop()
+    def save(leg_indices):
+        all_valid_leg_orders.append(leg_indices.copy())
     
+    current_leg_indices = [0]
+    increment_until_change(current_leg_indices)  # To make sure the first leg actually starts at the root_origin
+    # Now we create the while loop that uses these operations
+    while True:
+        # Did the last index cycle through every leg?
+        if current_leg_indices[-1] >= total_legs:
+            remove_last(current_leg_indices)
+            if len(current_leg_indices) == 0:  # Then we are done searching
+                break
+                
+            increment(current_leg_indices)
+            increment_until_change(current_leg_indices)
+        else:
+            last_leg_index = current_leg_indices[-1]
+            last_destination = leg_list[last_leg_index][1]
+            # Does the itinerary end at the destination?
+            if last_destination == root_destination:
+                save(current_leg_indices)
+                increment(current_leg_indices)
+                increment_until_change(current_leg_indices)
+            else:
+                # Then the itinerary is valid so far but incomplete
+                append_zero(current_leg_indices)
+                increment_until_change(current_leg_indices)
+
     return all_valid_leg_orders
 
 
@@ -140,89 +117,36 @@ def get_all_valid_leg_orders(root_origin, root_destination, leg_list):
 
 
 
-# Now reorganize the csv data into a dictionary that uses the (origin, destination, date) tuple to group flights.
+# Now reorganize the SQLite data into a dictionary that uses the (origin, destination, date) tuple to group flights.
 # We also find all the common layover flights at the same time, and organize them in the same way.
-# This relies on the CSV file being ordered so that every flight with some combination
+# This relies on the SQLite file being ordered so that every flight with some combination
 # (origin, destination, date) is adjacent to all the others; they can't be scattered randomly along the lists.
 # For the final product, we may want a better method for finding layover flights than just checking where the flights of the day have layovers,
 # but for now it works.
 
-original_flights = {}
+original_flights = dcl.unpack_flight_array(original_data_dict, id_keys=('origin', 'destination', 'date'))
 # Each (origin, destination, date) key of "original_flights" will lead to another dictionary of the form:
-    # {'airline_name': <name>,
-    #  'airline_code': <code>,
-    #  'total_amount': <amount>,
-    #  'total_currency' : <currency>,
-    #  'segments' : <segments>,
+    # {'airline_name': [<Flight 0 airline_name>, <Flight 1 airline_name>, ...],
+    #  'airline_code': [<Flight 0 airline_code>, <Flight 1 airline_code>, ...],
+    #  'total_amount': [<Flight 0 total_amount>, <Flight 1 total_amount>, ...],
+    #  'total_currency' : [<Flight 0 total_currency>, <Flight 1 total_currency>, ...],
+    #  'segments' : [<Flight 0 segments>, <Flight 1 segments>, ...],
     # }
-
-original_csv_length = len(original_data_dict['origin'])
-current_index = 0
-while current_index < original_csv_length:  # This will go until we exhaust the CSV file
-    identifiers = (original_data_dict['origin'][current_index],
-                   original_data_dict['destination'][current_index],
-                   original_data_dict['date'][current_index])
-    # We add this to the dictionary
-    original_flights[identifiers] = []
-
-    while True:  # This will go until it reaches a flight with a different origin, destination, or date
-        temp_data_dict = {'airline_name' : original_data_dict['airline_name'][current_index],
-                           'airline_code' : original_data_dict['airline_code'][current_index],
-                           'total_amount' : original_data_dict['total_amount'][current_index],
-                           'total_currency' : original_data_dict['total_currency'][current_index],
-                           'segments' : original_data_dict['segments'][current_index]}
-        original_flights[identifiers].append(temp_data_dict)
-
-        current_index += 1
-        if current_index >= original_csv_length or (original_data_dict['origin'][current_index],
-                                                    original_data_dict['destination'][current_index],
-                                                    original_data_dict['date'][current_index]) != identifiers:
-            break
 
 # We do something similar with routing_flights, but with extra nesting.
+routing_flights_roots = dcl.unpack_flight_array(routing_data_dict, id_keys=('root_origin', 'root_destination', 'date'))
 routing_flights = {}
+for key in routing_flights_roots.keys():
+    routing_flights[key] = dcl.unpack_flight_array(routing_flights_roots[key], id_keys=('origin', 'destination'))
 # Each (root_origin, root_destination, date) key of "routing_flights" will lead to another dictionary with keys (origin, destination),
 # which in turn lead to dictionaries of the form:
-    # {'airline_name': <name>,
-    #  'airline_code': <code>,
-    #  'total_amount': <amount>,
-    #  'total_currency' : <currency>,
-    #  'segments' : <segments>,
+    # {'airline_name': [<Flight 0 airline_name>, <Flight 1 airline_name>, ...],
+    #  'airline_code': [<Flight 0 airline_code>, <Flight 1 airline_code>, ...],
+    #  'total_amount': [<Flight 0 total_amount>, <Flight 1 total_amount>, ...],
+    #  'total_currency' : [<Flight 0 total_currency>, <Flight 1 total_currency>, ...],
+    #  'segments' : [<Flight 0 segments>, <Flight 1 segments>, ...],
     # }
 
-routing_csv_length = len(routing_data_dict['root_origin'])
-current_index = 0
-while current_index < routing_csv_length:  # This will go until we exhaust the CSV file
-    identifiers = (routing_data_dict['root_origin'][current_index],
-                   routing_data_dict['root_destination'][current_index],
-                   routing_data_dict['date'][current_index])
-    # We add this to the dictionary
-    routing_flights[identifiers] = {}
-
-    while True:  # This will go until it reaches a flight with a different root_origin, root_destination, or date
-        next_identifiers = (routing_data_dict['origin'][current_index],
-                            routing_data_dict['destination'][current_index])
-        
-        routing_flights[identifiers][next_identifiers] = []
-        while True:  # This will go until it reaches a flight with a different origin or destination
-
-            temp_data_dict = {'airline_name' : routing_data_dict['airline_name'][current_index],
-                            'airline_code' : routing_data_dict['airline_code'][current_index],
-                            'total_amount' : routing_data_dict['total_amount'][current_index],
-                            'total_currency' : routing_data_dict['total_currency'][current_index],
-                            'segments' : routing_data_dict['segments'][current_index]}
-            routing_flights[identifiers][next_identifiers].append(temp_data_dict)
-
-            current_index += 1
-
-            if current_index >= routing_csv_length or (routing_data_dict['origin'][current_index],
-                                                       routing_data_dict['destination'][current_index]) != next_identifiers:
-                break
-
-        if current_index >= routing_csv_length or (routing_data_dict['root_origin'][current_index],
-                                                   routing_data_dict['root_destination'][current_index],
-                                                   routing_data_dict['date'][current_index]) != identifiers:
-            break
 
 
 # Now both datasets are in useful formats, and we can search for synthetic routing. We can have a lot more detail for the final product, but for now,
@@ -232,17 +156,20 @@ while current_index < routing_csv_length:  # This will go until we exhaust the C
 # First we find the cheapest original flights, before we try synthetic routing
 original_cheapest_flights = {}
 for key in original_flights.keys():
-    original_cheapest_flights[key] = get_cheapest_flight_index_and_amount(original_flights[key])
+    min_amount = min(original_flights[key]['total_amount'])
+    min_index = original_flights[key]['total_amount'].index(min_amount)
+    original_cheapest_flights[key] = [min_index, min_amount]
 
 # Now cheapest routing flights
 routing_cheapest_flights = {}
 for key1 in routing_flights.keys():
     routing_cheapest_flights[key1] = {}
     for key2 in routing_flights[key1].keys():
-        routing_cheapest_flights[key1][key2] = get_cheapest_flight_index_and_amount(routing_flights[key1][key2])
+        min_amount = min(routing_flights[key1][key2]['total_amount'])
+        min_index = routing_flights[key1][key2]['total_amount'].index(min_amount)
+        routing_cheapest_flights[key1][key2] = [min_index, min_amount]
 
-# Oh... the flights are already sorted by price. Ah well, it doesn't hurt to sort them again. We can skip the "get_cheapest_flight_index_and_amount" part
-# later on if needed.
+# Oh... the flights are already sorted by price. Ah well, it doesn't hurt to sort them again. We can skip that later if needed
 
 for key1 in routing_flights.keys():
     direct_price = original_cheapest_flights[key1][1]
@@ -268,12 +195,14 @@ for key1 in routing_flights.keys():
         
         synthetic_prices.append(total_price)
     
-    result = get_cheapest_leg_order_index_and_amount(synthetic_prices)
-    cheapest_synthetic_leg_indices = all_valid_leg_orders[result[0]]
+    min_amount = min(synthetic_prices)
+    min_index = synthetic_prices.index(min_amount)
+
+    cheapest_synthetic_leg_indices = all_valid_leg_orders[min_index]
     cheapest_synthetic_legs = []
     for i in range(len(cheapest_synthetic_leg_indices)):
         cheapest_synthetic_legs.append(leg_list[cheapest_synthetic_leg_indices[i]])
-    synthetic_price = result[1]
+    synthetic_price = min_amount
 
     print(f'For flights from {key1[0]} to {key1[1]} on {key1[2]}:')
     print(f'    The cheapest direct flight is {direct_price}')
