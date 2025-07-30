741dc148a8645c182f8fce9a0bc8a82298fd46c3
mbhadriraju <151967712+mbhadriraju@users.noreply.github.com>
2025-07-30 13:06:22 -0500

    Add files via upload

---
+++ FlightRecSystem/query.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv("name.csv")
+
+airs = set(df["Airlines"])
+
+deps = set(df["Departures"])
+
+def get_results(arr, dep, air):
+    if air not in airs:
+        raise ValueError("Invalid Airline")
+    if dep not in deps:
+        raise ValueError("Invalid Departure")
+    arrs = set(df[df["Departures"] == dep]["Arrivals"])
+    if arr not in arrs:
+        raise ValueError("Invalid Arrival")
+    filt = df[(df["Arrivals"] == arr) & (df["Departures"] == dep) & (df["Airlines"] == air)]
+    if filt.empty:
+        raise ValueError("Invalid Combination")
+    return filt.values.to_list()[0]
